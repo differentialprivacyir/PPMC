971b87f8e620d78854e4f0ba842263cf219bd58e
arash <arash99s@yahoo.com>
2025-04-24 19:37:09 +0330

    Evaluation and Improvement

--- main.py
+++ main.py
@@ -2,36 +2,48 @@ from internal.initialize_data import *
 from client.client import *
 from server.server import *
 
-EPSILON = 10
+EPSILON = 1
 RADNOM_SEED = 10
+DATASET_NUMBER = 2
+B = 0.005
+DELTA = 0.001
 
 def main():
     # initialize
-    dataset = read_dataset("dataset/Data2-coarse.dat")
-    domains = attributes_domain("dataset/Data2-coarse.domain")
+    dataset = read_dataset(f'dataset/Data{DATASET_NUMBER}-coarse.dat')
+    domains = attributes_domain(f'dataset/Data{DATASET_NUMBER}-coarse.domain')
 
     # normalize to [-1,1]
     normalized_dataset = normalize_dataset(dataset, domains)
+    # normalized_dataset = dataset
 
     # Client
-    client_obj = Client(EPSILON, RADNOM_SEED)
-    new_eigenvector, new_avg = client_obj.send_perturbed_avg_eigenvector(normalized_dataset[0])
-
+    client_obj = Client(EPSILON, RADNOM_SEED, B, DELTA)
     # Server
     server_obj = Server(domains)
-    retrieval_data = server_obj.received_avg_eigenvector(new_avg, new_eigenvector)
 
+    # loop on data
+    retrieval_dataset = wheel_of_differential(server_obj, client_obj, normalized_dataset)
 
     # denormalizing
-    nor = []
-    nor.append(retrieval_data)
-    de = denormalize_dataset(nor, domains)
+    denormalized = denormalize_dataset(retrieval_dataset, domains)
 
-    print('original data:', dataset[0])
-    print('retrieval data:', de[0])
+    # evaulation
+    print('MSE is', findMSE(normalized_dataset, retrieval_dataset))
+    _, avg = average_variation_distance(dataset, denormalized)
+    print('Average Variation Distance is', avg)
 
-    print('MSE is', findMSE(dataset[0], de[0]))
 
+
+def wheel_of_differential(server_obj, client_obj, normalized_dataset):
+    print('Wheel of Differential ...')
+    retrieval_dataset = []
+    for data in normalized_dataset:
+        new_eigenvector, new_avg = client_obj.send_perturbed_avg_eigenvector(data)
+        retrieval_data = server_obj.received_avg_eigenvector(new_avg, new_eigenvector)
+        retrieval_dataset.append(retrieval_data)
+
+    return retrieval_dataset
 
 if __name__ == "__main__":
     main()
